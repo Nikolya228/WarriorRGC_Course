56f4be48d2d1d7ca0c8cdd07ab76c465b6c57850
Nikolya228 <kolya.vishnevskiy.3@gmail.com>
2025-02-13 22:48:31 +0200

    set up behaviour tree

--- Source/Warior/Private/Controllers/WarriorAIController.cpp
+++ Source/Warior/Private/Controllers/WarriorAIController.cpp
@@ -5,6 +5,7 @@
 #include "Navigation/CrowdFollowingComponent.h"
 #include "Perception/AIPerceptionComponent.h"
 #include "Perception/AISenseConfig_Sight.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 AWarriorAIController::AWarriorAIController(const FObjectInitializer& ObjectInitializer) 
 	: Super(ObjectInitializer.SetDefaultSubobjectClass<UCrowdFollowingComponent>(TEXT("PathFollowingComponent")))
@@ -43,6 +44,9 @@ void AWarriorAIController::OnEnemyPerceptionUpdated(AActor* Actor, FAIStimulus S
 {
 	if (Stimulus.WasSuccessfullySensed() && Actor)
 	{
-		UE_LOG(LogTemp, Error, TEXT("Successfuly sensed"));
+		if (UBlackboardComponent* BlackboardComponent = GetBlackboardComponent())
+		{
+			BlackboardComponent->SetValueAsObject(FName("TargetActor"), Actor);
+		}
 	}
 }
