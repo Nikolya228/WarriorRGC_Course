21e0610dfd87c51e0ea6538dcc63c0eee4a8992e
Nikolya228 <kolya.vishnevskiy.3@gmail.com>
2025-02-13 22:18:37 +0200

    set up enemy controller path component && set up enemy ai perception

---
+++ Source/Warior/Private/Controllers/WarriorAIController.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Controllers/WarriorAIController.h"
+#include "Navigation/CrowdFollowingComponent.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
+
+AWarriorAIController::AWarriorAIController(const FObjectInitializer& ObjectInitializer) 
+	: Super(ObjectInitializer.SetDefaultSubobjectClass<UCrowdFollowingComponent>(TEXT("PathFollowingComponent")))
+{
+	if (UCrowdFollowingComponent* CrowdComp = Cast<UCrowdFollowingComponent>(GetPathFollowingComponent()))
+	{
+	}
+
+	AISenseConfig_Sight = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("EnemySenseConfig_Sight"));
+	AISenseConfig_Sight->DetectionByAffiliation.bDetectEnemies = true;
+	AISenseConfig_Sight->DetectionByAffiliation.bDetectFriendlies = false;
+	AISenseConfig_Sight->DetectionByAffiliation.bDetectNeutrals = false;
+	AISenseConfig_Sight->SightRadius = 5000.f;
+	AISenseConfig_Sight->LoseSightRadius = 0.f;
+	AISenseConfig_Sight->PeripheralVisionAngleDegrees = 360.f;
+
+	EnemyPerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("EnemyPerceptionComponent"));
+	EnemyPerceptionComponent->ConfigureSense(*AISenseConfig_Sight);
+	EnemyPerceptionComponent->SetDominantSense(UAISenseConfig_Sight::StaticClass());
+	EnemyPerceptionComponent->OnTargetPerceptionUpdated.AddDynamic(this, &AWarriorAIController::OnEnemyPerceptionUpdated);
+}
+
+void AWarriorAIController::OnEnemyPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
+{
+}
