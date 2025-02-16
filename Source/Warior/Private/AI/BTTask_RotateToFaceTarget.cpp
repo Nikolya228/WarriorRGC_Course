c631a09ece6b6b9b429a6417f99639a4282f868c
Nikolya228 <kolya.vishnevskiy.3@gmail.com>
2025-02-16 19:29:16 +0200

    construct native BTTask

---
+++ Source/Warior/Private/AI/BTTask_RotateToFaceTarget.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/BTTask_RotateToFaceTarget.h"
+
+UBTTask_RotateToFaceTarget::UBTTask_RotateToFaceTarget()
+{
+	NodeName = TEXT("Native Rotate To Face Target Actor");
+	AnglePrecision = 10.f;
+	RotationInterpSpeed = 5.f;
+
+	bNotifyTick = true;
+	bNotifyTaskFinished = true;
+	bCreateNodeInstance = false;
+
+	INIT_TASK_NODE_NOTIFY_FLAGS();
+
+	InTargetToFaceKey.AddObjectFilter(this, GET_MEMBER_NAME_CHECKED(ThisClass, InTargetToFaceKey), AActor::StaticClass());
+}
+
+void UBTTask_RotateToFaceTarget::InitializeFromAsset(UBehaviorTree& Asset)
+{
+	Super::InitializeFromAsset(Asset);
+
+	if (UBlackboardData* BBAsset = GetBlackboardAsset())
+	{
+		InTargetToFaceKey.ResolveSelectedKey(*BBAsset);
+	}
+}
+
+uint16 UBTTask_RotateToFaceTarget::GetInstanceMemorySize() const
+{
+	return sizeof(FRotateToFaceTargetTaskMemory);
+}
+
+FString UBTTask_RotateToFaceTarget::GetStaticDescription() const
+{
+	const FString KeyDescription = InTargetToFaceKey.SelectedKeyName.ToString();
+	return FString::Printf(TEXT("Smoothly rotates to face %s until the angle precision %s is reached"), *KeyDescription, *FString::SanitizeFloat(AnglePrecision));
+}
