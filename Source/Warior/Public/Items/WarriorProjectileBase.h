2df2e071748a4bc8c4ba9dd992de06c85eeb7983
Nikolya228 <kolya.vishnevskiy.3@gmail.com>
2025-08-04 16:45:43 +0300

    created base projectile class

---
+++ Source/Warior/Public/Items/WarriorProjectileBase.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "WarriorProjectileBase.generated.h"
+
+class UBoxComponent;
+class UNiagaraComponent;
+class UProjectileMovementComponent;
+
+UENUM(BlueprintType)
+enum class EProjectileDamagePolicy : uint8
+{
+	OnHit,
+	OnBeginOverlap
+};
+
+UCLASS()
+class WARIOR_API AWarriorProjectileBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AWarriorProjectileBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Projectile")
+	UBoxComponent* ProjectileCollisionBox;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Projectile")
+	UNiagaraComponent* ProjectileNiagaraComponent;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Projectile")
+	UProjectileMovementComponent* ProjectileMovementComp;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Projectile")
+	EProjectileDamagePolicy ProjectileDamagePolicy = EProjectileDamagePolicy::OnHit;
+};
