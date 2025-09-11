57eec8dc779a4cbbfd082cc7d2cf79481e862262
Nikolya228 <kolya.vishnevskiy.3@gmail.com>
2025-09-11 03:55:38 +0300

    created ability task wait spawn enemies

---
+++ Source/Warior/Public/AbilitySystem/AbilityTasks/AbilityTask_WaitSpawnEnemies.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "AbilityTask_WaitSpawnEnemies.generated.h"
+
+class AWarriorEnemyBaseCharacter;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FWaitSpawnEnemiesDelegate, const TArray<AWarriorEnemyBaseCharacter*>&, SpawnedEnemies);
+
+/**
+ * 
+ */
+UCLASS()
+class WARIOR_API UAbilityTask_WaitSpawnEnemies : public UAbilityTask
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable, Category = "Warrior|AbilityTasks", meta = (DisplayName = "Wait Gameplay Event And Spawn Enemies", HidePin = "OwningAbility", DefaultToSelf = "OwningAbility", BlueprintInternalUseOnly = "true", NumToSpawn = "1", RandomSpawnRadius = "200"))
+	static UAbilityTask_WaitSpawnEnemies* WaitSpawnEnemies(
+		UGameplayAbility* OwningAbility,
+		FGameplayTag EventTag, 
+		TSoftClassPtr<AWarriorEnemyBaseCharacter> SoftEnemyClassToSpawn, 
+		int32 NumToSpawn, 
+		const FVector& SpawnOrigin, 
+		const float RandomSpawnRadius, 
+		const FRotator& SpawnRotation);
+
+	UPROPERTY(BlueprintAssignable)
+	FWaitSpawnEnemiesDelegate OnSpawnFinished;
+
+	UPROPERTY(BlueprintAssignable)
+	FWaitSpawnEnemiesDelegate DidNotSpawn;
+
+private:
+	FGameplayTag CachedEventTag;
+	TSoftClassPtr<AWarriorEnemyBaseCharacter> CachedSoftEnemyClassToSpawn;
+	int32 CachedNumToSpawn;
+	FVector CachedSpawnOrigin;
+	float CachedRandomSpawnRadius;
+	FRotator CachedSpawnRotation;
+};
