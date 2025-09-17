79a77609692c9220f912c3665dd8098c40582ff3
Nikolya228 <kolya.vishnevskiy.3@gmail.com>
2025-09-17 08:42:50 +0300

    Consume Stones

--- Source/Warior/Private/Items/PickUps/WarriorStoneBase.cpp
+++ Source/Warior/Private/Items/PickUps/WarriorStoneBase.cpp
@@ -6,6 +6,17 @@
 #include "AbilitySystem/WarriorAbilitySystemComponent.h"
 #include "WariorGameplayTags.h"
 
+void AWarriorStoneBase::Consume(UWarriorAbilitySystemComponent* AbilitySystemComponent, int32 ApplyLevel)
+{
+	check(StoneGameplayEffectClass);
+
+	UGameplayEffect* EffectCDO = StoneGameplayEffectClass->GetDefaultObject<UGameplayEffect>();
+
+	AbilitySystemComponent->ApplyGameplayEffectToSelf(EffectCDO, ApplyLevel, AbilitySystemComponent->MakeEffectContext());
+
+	BP_OnStoneConsumed();
+}
+
 void AWarriorStoneBase::OnPickUpCollisionSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	if (AWarriorHeroCharacter* OverlappedHeroCharacter = Cast<AWarriorHeroCharacter>(OtherActor))
